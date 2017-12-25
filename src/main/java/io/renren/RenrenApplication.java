7ae4c282cea5ea833c319868a1fc42999107c91c
sunlightcs <sunlightcs@gmail.com>
2017-12-25 21:56:32 +0800

    优化

--- src/main/java/io/renren/RenrenApplication.java
+++ src/main/java/io/renren/RenrenApplication.java
@@ -1,12 +1,16 @@
 package io.renren;
 
+import io.renren.datasources.DynamicDataSourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@Import({DynamicDataSourceConfig.class})
 public class RenrenApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
